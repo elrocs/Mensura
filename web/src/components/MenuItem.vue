9c6cd7600445be0a0230a25b58aefeb5a5d69312
elrocas <4536rty789ui@tuta.io>
2025-02-15 22:48:11 +0200

    add menuitem

---
+++ web/src/components/MenuItem.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+    import {
+        Card,
+        CardTitle,
+    } from '@/components/ui/card'
+
+</script>
+
+<template>
+    <Card>
+        <CardTitle>Yo mandem</CardTitle>
+    </Card>
+</template>
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
+  import MenuItem from '@/components/MenuItem.vue';
 </script>
 
-<template>
-  ahfalskjdahlksjdfhalkjd
+<template >
+  <div class="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-6 lg:grid-cols-7 gap-4">
+    <MenuItem class="aspect-square"></MenuItem>
+    <MenuItem class="aspect-square"></MenuItem>
+    <MenuItem class="aspect-square"></MenuItem>
+    <MenuItem class="aspect-square"></MenuItem>
+    <MenuItem class="aspect-square"></MenuItem>
+    <MenuItem class="aspect-square"></MenuItem>
+    <MenuItem class="aspect-square"></MenuItem>
+    <MenuItem class="aspect-square"></MenuItem>
+    <MenuItem class="aspect-square"></MenuItem>
+    <MenuItem class="aspect-square"></MenuItem>
+    <MenuItem class="aspect-square"></MenuItem>
+    <MenuItem class="aspect-square"></MenuItem>
+    <MenuItem class="aspect-square"></MenuItem>
+  </div>
 </template>
