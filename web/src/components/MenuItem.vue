1e1e7fe6c3ec587f6cfd83acfa41fa01d1f6fe4c
elrocs <rocvssdd@gmail.com>
2025-02-18 18:30:02 +0200

    feat(HomeItem): add logo and title

--- web/src/components/MenuItem.vue
+++ web/src/components/MenuItem.vue
@@ -2,11 +2,24 @@
 
     import {
         Card,
+        CardTitle,
     } from '@/components/ui/card'
+
+    import { Icon } from '@iconify/vue'
+
+    defineProps<{icon: string}>()
 
 </script>
 
 <template>
-    <Card>
+    <Card class="relative aspect-square flex items-center justify-center p-4 group cursor-pointer">
+      
+      <Icon :icon="icon" class="w-[50%] h-[50%] text-muted-foreground" />
+  
+      <CardTitle class="absolute bottom-2 right-2 text-muted-foreground text-sm opacity-0 transition-opacity duration-100 group-hover:opacity-100">
+        <slot />
+      </CardTitle>
+  
     </Card>
-</template>
+  </template>
+  
